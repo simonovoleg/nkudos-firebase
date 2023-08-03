68cda6088eb0a751b6769a0359d02c3800ba7053
Hoyong Jung <hjung@simplenexus.com>
2023-08-03 15:30:54 -0600

    leaderboard. WIP: we need to think about how we want to handle if more than 1 person has the same points

--- commands/leaderboard.js
+++ commands/leaderboard.js
@@ -3,16 +3,48 @@ const {getUsers} = require('../datastore/service')
 module.exports = async ({body, client, logger}) => {
   try {
     const users = await getUsers();
-    console.log(users);
 
-    users.forEach(function (user) {
-      var x = user.nkudos_received;
-      console.log(x);
+    const currentMonth = getCurrentMonth();
+    const filteredData = users.filter(user => {
+      return user.nkudos_received.some(kudo => kudo.timestamp.startsWith(currentMonth));
     });
+    
+    // Group the data by 'value' category and calculate the total kudos received by each user for each category
+    const kudosByValue = {};
+    filteredData.forEach(user => {
+      user.nkudos_received.forEach(kudo => {
+        if (!kudosByValue[kudo.value]) {
+          kudosByValue[kudo.value] = {};
+        }
+        if (!kudosByValue[kudo.value][user.id]) {
+          kudosByValue[kudo.value][user.id] = 0;
+        }
+        kudosByValue[kudo.value][user.id]++;
+      });
+    });
+
+    const mostKudosByValue = {};
+    for (const value in kudosByValue) {
+      let maxKudos = 0;
+      let userIdWithMostKudos = null;
+      for (const userId in kudosByValue[value]) {
+        const kudosCount = kudosByValue[value][userId];
+        if (kudosCount > maxKudos) {
+          maxKudos = kudosCount;
+          userIdWithMostKudos = userId;
+        }
+      }
+      mostKudosByValue[value] = userIdWithMostKudos;
+    }
+
+    msg = ':sports_medal: Leaderboard of the month :sports_medal:'
+    for (const e in mostKudosByValue) {
+      msg += `\n>*${e}*: <@${mostKudosByValue[e]}>`
+    }
     
     try {
       const result = await client.chat.postMessage({
-        text: 'Hello world',
+        text: msg,
         channel: 'C05LWDZV83S'
       });
 
@@ -24,3 +56,10 @@ module.exports = async ({body, client, logger}) => {
     logger.error(error);
   }
 };
+
+function getCurrentMonth() {
+  const today = new Date();
+  const year = today.getFullYear();
+  const month = today.getMonth() + 1;
+  return `${year}-${month.toString().padStart(2, '0')}`;
+}
