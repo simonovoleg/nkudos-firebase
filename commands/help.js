642c3ee00ce2044704db113d4e8d36f928cfbfd5
Tanner Holle <tholle@simplenexus.com>
2023-08-03 16:34:48 -0600

    updated the help

--- commands/help.js
+++ commands/help.js
@@ -3,10 +3,9 @@ const { getUser } = require('../datastore/service');
 module.exports = async ({body, client, logger}) => {
   try {
     let message = '';
-      message += 'We are excited for you to use nKudos! Here are the possible commands: \n' +
-      `\n> *give*: Open a form to recognize a peer for Living the Six` +
-      `\n> *balance*: Check how many nKudos you can give this month, and how many you've received` +
-      `\n> *leaderboard*: See which of your peers has received the most nKudos in each category`;
+      message += 'We are excited for you to use nKudos! Here are the possible commands:' +
+      `\n> *give (g)*: Open a form to recognize a peer for Living the Six` +
+      `\n> *balance (b)*: Check how many nKudos you can give this month, and how many you've received`;
     try {
       await client.chat.postEphemeral({
         text: message,
